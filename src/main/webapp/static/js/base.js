187941de434b36b9ed0e862429694d163f36f321
dirkraft <dirkraft@gmail.com>
2013-04-11 11:12:39 -0500

    don't add to Object prototype

--- src/main/webapp/static/js/base.js
+++ src/main/webapp/static/js/base.js
@@ -1,24 +1,24 @@
-Object.prototype.keys = function () {
-    var keys = [];
-    for(var i in this) if (this.hasOwnProperty(i)) {
-        keys.push(i);
-    }
-    return keys;
-};
-
-String.prototype.format = function() {
+String.prototype.format = function () {
     var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) {
+    return this.replace(/{(\d+)}/g, function (match, number) {
         return typeof args[number] != 'undefined' ? args[number] : match;
     });
 };
 
+function keys(obj) {
+    var keys = [];
+    for (var i in obj) if (obj.hasOwnProperty(i)) {
+        keys.push(i);
+    }
+    return keys;
+}
+
 /**
-* Logs a warning if the condition is false (an assertion fails).
-*
-* @param {boolean} condition
-* @param {String} failMessage
-*/
+ * Logs a warning if the condition is false (an assertion fails).
+ *
+ * @param {boolean} condition
+ * @param {String} failMessage
+ */
 function assertSoft(condition, failMessage) {
     if (!condition) {
         console.log(failMessage);
@@ -27,39 +27,38 @@ function assertSoft(condition, failMessage) {
 
 
 /**
-* @param control may be any type which is accepted by jQuery's $( ... )
-*/
+ * @param control may be any type which is accepted by jQuery's $( ... )
+ */
 function lockControl(control) {
     $(control).attr('disabled', 'disabled');
 }
 /**
-* @param container may be any type which is accepted by jQuery's $( ... )
-*/
+ * @param container may be any type which is accepted by jQuery's $( ... )
+ */
 function lockForms(container) {
     container = container || $('body');
     $(container).find('input:not(.disabled),button:not(.disabled),select:not(.disabled)').attr('disabled', 'disabled');
 }
 
 /**
-* @param control may be any type which is accepted by jQuery's $( ... )
-*/
+ * @param control may be any type which is accepted by jQuery's $( ... )
+ */
 function unlockControl(control) {
     $(control).removeAttr('disabled');
 }
 /**
-* @param container may be any type which is accepted by jQuery's $( ... )
-*/
+ * @param container may be any type which is accepted by jQuery's $( ... )
+ */
 function unlockForms(container) {
     container = container || $('body');
     $(container).find('input:not(.disabled),button:not(.disabled),select:not(.disabled)').removeAttr('disabled');
 }
 
 /** turn a form into a js object */
-$.fn.serializeObject = function()
-{
+$.fn.serializeObject = function () {
     var o = {};
     var a = this.serializeArray();
-    $.each(a, function() {
+    $.each(a, function () {
         if (o[this.name] !== undefined) {
             if (!o[this.name].push) {
                 o[this.name] = [o[this.name]];
@@ -73,7 +72,7 @@ $.fn.serializeObject = function()
 };
 
 // default $.ajax error function
-$(document).ajaxError(function(event, jqXHR, ajaxSettings, thrownError) {
+$(document).ajaxError(function (event, jqXHR, ajaxSettings, thrownError) {
     var exception, exceptMsg;
     if (jqXHR.responseText) {
         try {
